ab9683f027c84ce2312243a86dccfe065a1b1dd2
JWLD <jackwldavies@icloud.com>
2017-08-12 09:26:41 +0300

    set up project

---
+++ src/start.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const server = require('./server');
+
+// start the server
+server.start((err) => {
+  if (err) throw err;
+
+  console.log(`Incredible things happening on port ${server.info.port}!`);
+});
