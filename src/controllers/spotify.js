030601b1f43c342c9037126a5a34bd5d11e67100
JWLD <jackwldavies@icloud.com>
2017-09-30 12:10:37 +0300

    extract and send album data to frontend #7

--- src/controllers/spotify.js
+++ src/controllers/spotify.js
@@ -1,5 +1,5 @@
 const Cookie = require('cookie');
-const Jwt = require('jsonwebtoken');
+const JsonWebToken = require('jsonwebtoken');
 const Request = require('request');
 
 const spotifyController = module.exports = {};
@@ -12,11 +12,11 @@ spotifyController.findAlbum = {
     const jwt = Cookie.parse(request.headers.cookie || '').jwt;
     if (!jwt) return reply('No access token provided').code(401);
 
-    const access_token = Jwt.verify(jwt, process.env.SECRET).access_token;
+    const access_token = JsonWebToken.verify(jwt, process.env.SECRET).access_token;
 
     // build request
     const options = {
-      url: `https://api.spotify.com/v1/search?type=album&q=artist:Hans%20Zimmer%20album:Paperhouse`,
+      url: `https://api.spotify.com/v1/search?type=album&q=artist:John%20Powell%20album:Fair%20Game`,
       json: true,
       headers: {
         Authorization: `Bearer ${access_token}`
@@ -27,7 +27,16 @@ spotifyController.findAlbum = {
     Request(options, (err, response, body) => {
       if (err) return reply(`Spotify Album Search Error: ${err}`).code(500);
 
-      console.log(body.albums.items);
+      const topResult = body.albums.items[0];
+
+      if (topResult) {
+        return reply({
+          id: topResult.id,
+          imgUrl: topResult.images[1].url
+        });
+      } else {
+        return reply('No albums found on Spotify');
+      }
     });
   }
 }
